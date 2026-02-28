ae90c09462c0bdfbfa87b08b9709f20f60d1023d
brauliocebuc <brauliocebuc@gmail.com>
2026-02-28 16:31:31 -0600

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,16 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = "= 1.6.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 1.0.0, < 7.0.0"
     }
   }
 }
 
 provider "aws" {
   region  = var.aws_region
-  profile = "curso_mlops"
 }
 
 # ─────────────────────────────────────────────
